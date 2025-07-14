ad1508853d03278cb90b6f8dbd426d429efcfcba
LAKSHMI NARASIMHULU MANCHI <m.narasimhans5974@gmail.com>
2025-07-14 14:57:41 +0530

    SUM OF ARRAY ELEMENTS.java

---
+++ SUM OF ARRAY ELEMENTS.java
@@ -0,0 +1,14 @@
+class sum_arrele
+{
+    	public static void main(String[] args) 
+	{
+		int sum=0;
+		int[] arr={10,20,30,40,50};
+			for(int i=0;i<=4;i++)
+			{
+			sum=sum+arr[i];
+			}
+		System.out.println(sum);
+	}
+}
+		
