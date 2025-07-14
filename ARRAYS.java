fa047d6ffc23893378fcf4dbabd75c5928c38dac
LAKSHMI NARASIMHULU MANCHI <m.narasimhans5974@gmail.com>
2025-07-14 13:52:26 +0530

    ARRAYS.java

---
+++ ARRAYS.java
@@ -0,0 +1,8 @@
+class arrays
+{
+	public static void main(String[] args)
+	{
+		int[] arr={10,20,30,40,50};    //array initilization
+		System.out.println(arr.length);        //to find array size
+	}
+}
