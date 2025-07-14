cfdece26ecdb9236929d3b9b0efeb48f4ebbdf54
LAKSHMI NARASIMHULU MANCHI <m.narasimhans5974@gmail.com>
2025-07-14 15:26:18 +0530

    ARRAY ELEMENTS BY USER.java

---
+++ ARRAY ELEMENTS BY USER.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class arrele {
+    public static void main(String[] args) {
+        String x;
+        String y;
+        Scanner sr = new Scanner(System.in);
+        System.out.println("ENTER X AND Y VALUES:");
+        x = sr.nextLine();
+        y = sr.nextLine();
+        System.out.println(x + " " + y);
+    }
+}
