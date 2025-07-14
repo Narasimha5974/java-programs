5c1486d19b235cdc41b7998400d9c73ad7ef0dd8
LAKSHMI NARASIMHULU MANCHI <m.narasimhans5974@gmail.com>
2025-07-14 13:49:55 +0530

    DTJAVA.java

---
+++ DTJAVA.java
@@ -0,0 +1,22 @@
+class dtjava
+{
+	public static void main(String[] args)
+	{
+		Boolean bln=true;
+		byte b=20;
+		short srt=120;
+		int t=1290;
+		long lg=12908;
+		float flt=3.5f;
+		double dl=23.4;
+		char ch='a';
+		System.out.println(bln);
+		System.out.println(b);
+		System.out.println(srt);
+		System.out.println(t);
+		System.out.println(lg);
+		System.out.println(flt);
+		System.out.println(dl);
+		System.out.println(ch);	
+	}
+}
