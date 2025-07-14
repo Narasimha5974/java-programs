22f3678539894154b92ab2b586fdbd68b654fb4d
LAKSHMI NARASIMHULU MANCHI <m.narasimhans5974@gmail.com>
2025-07-14 13:49:15 +0530

    RELATIONAL OPERATOR.java

---
+++ RELATIONAL OPERATOR.java
@@ -0,0 +1,12 @@
+class relational_operator
+{
+	public static void main(String[] args)
+	{
+		int x=100;
+		int y=200;
+		System.out.println("GREATER THAN: "+(x>y));
+		System.out.println("GREATER THAN OR EQUAL TO: "+(x>=y));
+		System.out.println("LESS THAN: "+(x<y));
+		System.out.println("LESS THAN OR EQUAL TO: "+(x<=y));
+	}
+}
