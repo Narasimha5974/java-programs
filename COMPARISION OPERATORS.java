3faa3f5697de7d31671d97ded21f55ec0518ea8c
LAKSHMI NARASIMHULU MANCHI <m.narasimhans5974@gmail.com>
2025-07-14 13:50:46 +0530

    COMPARISION OPERATORS.java

---
+++ COMPARISION OPERATORS.java
@@ -0,0 +1,10 @@
+class comparision_operator
+{
+	public static void main(String[] args)
+	{
+		int x=100;
+		int y=200;
+		System.out.println("GREATER THAN: "+(x==y));
+		System.out.println("GREATER THAN OR EQUAL TO: "+(x!=y));
+	}
+}
