a6e0c317a9a4852887e49041d194146653dcb065
LAKSHMI NARASIMHULU MANCHI <m.narasimhans5974@gmail.com>
2025-07-14 14:29:08 +0530

    ARRAYS USING FOR EACH.java

---
+++ ARRAYS USING FOR EACH.java
@@ -0,0 +1,10 @@
+class arr_foreach
+{
+    	public static void main(String[] args) 
+	{
+		int[] arr = {10, 20, 30, 40, 50, 60, 70};
+		for (int i : arr) {
+		System.out.println(i);  // Directly print each element
+        }
+    }
+}
