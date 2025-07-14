5645bb1000d2ff834a518b23506a5b357a2422e7
LAKSHMI NARASIMHULU MANCHI <m.narasimhans5974@gmail.com>
2025-07-14 13:51:59 +0530

    ARITHMETIC OPERATION.java

---
+++ ARITHMETIC OPERATION.java
@@ -0,0 +1,13 @@
+class arthop
+{
+	public static void main(String[] args)
+	{
+		int x=100;
+		int y=200;
+		System.out.println("ADDITION: "+(x+y));
+		System.out.println("SUBTRACTION: "+(x-y));
+		System.out.println("MULTIPLICATION: "+(x*y));
+		System.out.println("DIVISION: "+(x/y));
+		System.out.println("MODULO: "+(x%y));
+	}
+}
