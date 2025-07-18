5b2df7942437526becc92b69eed7cc8b73c148b1
LAKSHMI NARASIMHULU MANCHI <m.narasimhans5974@gmail.com>
2025-07-18 13:23:10 +0530

    HELLO.java

--- HELLO.java
+++ HELLO.java
@@ -1,7 +1,7 @@
-class hello
+class Hello
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World");
-    }
+     public static void main(String[] args)
+      {
+         System.out.println("Hello World");
+      }
 }
