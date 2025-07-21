293d650ed4f703507e03b042b76e4309719c30a2
LAKSHMI NARASIMHULU MANCHI <m.narasimhans5974@gmail.com>
2025-07-21 15:09:05 +0530

    Add files via upload

---
+++ arrayfirstlastele.java
@@ -0,0 +1,8 @@
+class arrayfirstlastele {
+    public static void main(String[] args){
+        int[] arr = {10, 20, 30, 40, 50}; 
+        int n = arr.length;            
+        System.out.println("first element: " + arr[0]);
+        System.out.println("last element: " + arr[n-1]);
+    }
+}
